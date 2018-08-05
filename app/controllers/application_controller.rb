3567de407819f7aa3e74a74d9bb522ad4bdf9cdf
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-05 13:49:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,9 +38,9 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  #get '/recipes/new' do
-  #  erb :new
-  #end
+  get '/recipes/new' do
+    erb :new
+  end
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
